fdaf5e7a7a121138ce5112466c2f84d3f85bc29a
vislee <louivislee@gmail.com>
2016-03-10 14:13:10 +0800

    insert sort

--- algorithm01.go
+++ algorithm01.go
@@ -52,6 +52,34 @@ func quick_sort(array []int, left, right int) {
 	}
 }
 
+// 插入排序
+func insert_sort(a []int) {
+	l := len(a)
+	for i := 1; i < l; i++ {
+		k := a[i]
+		j := 0
+		for j = i - 1; j >= 0 && a[j] > k; j-- {
+			a[j+1] = a[j]
+		}
+		a[j+1] = k
+	}
+}
+
+// 冒泡排序
+func bubble_sort(a []int) {
+	var i, j int
+	f := true
+	for i = 0; i < len(a) && f; i++ {
+		f = false
+		for j = len(a) - 1; j > i; j-- {
+			if a[j-1] > a[j] {
+				a[j-1], a[j] = a[j], a[j-1]
+				f = true
+			}
+		}
+	}
+}
+
 func main() {
 	test := []int{5, 3, 2, 4, 1, 6, 7, 15, 8, 10, 30, 9, 18, 23, 32, 21, 24, 27, 29}
 	quick_sort(test, 0, 18)
